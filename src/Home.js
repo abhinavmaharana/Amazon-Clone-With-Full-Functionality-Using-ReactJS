200bc63aa89580c1e4d82e2a4da98aeee2c65f15
abhinavmaharana <abhinavmaharana.004@gmail.com>
2020-09-09 16:59:02 +0530

    fixes

---
+++ src/Home.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import './Home.css';
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img
+                    className="home__image"
+                    src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/Xiaomi/Redmi_Note9Pro_Max/New_Creative/Desktop_TallHero_3000x1200._CB405044906_.jpg"
+                    alt=""
+                />
+
+                {/* Product id, title, price, rating, image */}
+                <div className="home__row">
+                    <Product
+                        id="12321341"
+                        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+                        price={1170.62}
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product
+                        id="12321348"
+                        title="HP Pavilion Gaming DK0268TX 15.6-inch Laptop (Core i5-9300H/8GB/512GB SSD/Windows 10 Home/4GB NVIDIA GeForce GTX 1650 Graphics), Shadow Black"
+                        price={67, 490.00}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/41Hxk3Dl5RL.__AC_SY200_.jpg"
+                    />
+                    <Product
+                        id="49538094"
+                        title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                        price={23443.94}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+                    />
+                    <Product
+                        id="49538097"
+                        title="Apple iPhone 11 (64GB)-(Product) RED"
+                        price={61900.00}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/41c2gS6Z+LL.__AC_SY200_.jpg"
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product
+                        id="4903850"
+                        title="Fitbit Charge 3 Fitness Activity Tracker Special Edition"
+                        price={19618.36}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61RJmfJ%2Bc0L._SX679_.jpg"
+                    />
+                    <Product
+                        id="4903852"
+                        title="Optimum Nutrition (ON) Gold Standard 100% Whey Protein Powder - 2 lbs, 907 g (Double Rich Chocolate), Primary Source Isolate"
+                        price={2773.00}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/51B1HVTHWcL.__AC_SY200_.jpg"
+                    />
+                    <Product
+                        id="23445930"
+                        title="Amazon Echo (3rd Gen) – Improved sound, powered by Dolby (Black) "
+                        price={9710.11}
+                        rating={5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/7128HRanJgL._SX425_.jpg"
+                    />
+                    <Product
+                        id="3254354345"
+                        title="Apple iPad Pro (11-inch, Wi-Fi, 128GB) - Silver (2nd Generation) "
+                        price={58756.0}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/815a%2BXjrgvL._SX466_.jpg"
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product
+                        id="90829332"
+                        title="OnePlus 138.8 cm (55 inches) Q1 Series 4K Certified Android QLED TV 55Q1IN Pro (Black)"
+                        price={107408.55}
+                        rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/71D89swgrvL._SX355_.jpg"
+                    />
+                    <Product
+                        id="75666462"
+                        title="Sennheiser Momentum True Wireless 2 - Bluetooth Earbuds with Active Noise Cancellation, Smart Pause, Customizable Touch Control and 28-Hour Battery Life - White"
+                        price={24990.00}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/71wwV3yyq3L._AC_UY218_.jpg"
+                    />
+                </div>
+
+                <div className="home__row">
+                    <Product
+                        id="90827332"
+                        title="Home Centre Silas Lounge Chair with Footrest Black"
+                        price={35400}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/81GRpFfD2GL._AC_UL320_.jpg"
+                    />
+                    <Product
+                        id="75466462"
+                        title="MISURAA Imported Shadow Grey Ergonomic Chair with Mesh Back and Foam Seat for Office "
+                        price={11999}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/61XEeDats+L._AC_UL320_.jpg"
+                    />
+                    <Product
+                        id="75466464"
+                        title="Apple MacBook Pro "
+                        price={199900}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/71L2iBSyyOL._AC_UY218_.jpg"
+                    />
+                    <Product
+                        id="75466463"
+                        title="Canon EOS 1500D 24.1 Digital SLR Camera (Black)"
+                        price={31490}
+                        rating={5}
+                        image="https://m.media-amazon.com/images/I/914hFeTU2-L._AC_UY218_.jpg"
+                    />
+                    <Product
+                        id="75466467"
+                        title="Blue Yeti USB Mic"
+                        price={12499}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/81Ac7KMx7RL._AC_UL320_.jpg"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAC13EU2T1ExIznJ5RfR_ev8aSUfJ2YvcM",
+    authDomain: "challenge-88129.firebaseapp.com",
+    databaseURL: "https://challenge-88129.firebaseio.com",
+    projectId: "challenge-88129",
+    storageBucket: "challenge-88129.appspot.com",
+    messagingSenderId: "757023977578",
+    appId: "1:757023977578:web:5e16922aba6b8fad3c9a13",
+    measurementId: "G-GCMXCNVJVL"
+};
